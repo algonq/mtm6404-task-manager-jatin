110c64fc967a0475dc06e63f803473c138d90d7f
jatin90541117633 <134572776+algonq@users.noreply.github.com>
2024-10-16 18:18:11 -0400

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+
+// Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Task Manager. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
